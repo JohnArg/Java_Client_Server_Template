bd2f64f9308fa22ba2deb9265786f9a64db7b079
John Argyroulis <johnargyroulis@gmail.com>
2018-11-26 16:11:31 +0100

    Worker Per Request done. To be tested

--- Java_MTHR_Server/src/mt_server/main/ServerApp.java
+++ Java_MTHR_Server/src/mt_server/main/ServerApp.java
@@ -1,10 +1,14 @@
 package mt_server.main;
 
+import mt_server.server_utils.TCPServer;
+import mt_server.server_utils.WPR_TCP_Server;
+
 public class ServerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int port = 3000;
+		TCPServer server = new WPR_TCP_Server(port);
+		server.startServer();
 	}
 
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+Java Client Server Template
+==
+Multithreaded TCP Server
+--
+____
+Java 1.8
+
+Template code implementing the client-server model with TCP and multithreading. The server can either use Worker Per Request or a Thread Pool. Default functionality is to echo to the client what they sent. To be used for a quick setup. 
